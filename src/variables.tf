40629324d54f3da26f00d5793f101056018c1784
mspitsyn <mgraf@yandex.ru>
2025-07-10 17:19:01 +0300

    Добавил решение 1

---
+++ src/variables.tf
@@ -0,0 +1,40 @@
+###cloud vars
+
+
+variable "cloud_id" {
+  type        = string
+  default = "b1gchhnthvn0qe3v2rfb"
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
+}
+
+variable "folder_id" {
+  type        = string
+  default = "b1gh2n9lu2pm397sqa8m"
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
+}
+
+variable "default_zone" {
+  type        = string
+  default     = "ru-central1-a"
+  description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
+}
+variable "default_cidr" {
+  type        = list(string)
+  default     = ["10.0.1.0/24"]
+  description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
+}
+
+variable "vpc_name" {
+  type        = string
+  default     = "develop"
+  description = "VPC network & subnet name"
+}
+
+
+###ssh vars
+
+variable "vms_ssh_root_key" {
+  type        = string
+  default     = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEHRcJFPPYCyIbTvpZaN0Lm7YDV0CupCsj1RBJ+KMTGD mspitsin@mspitsin"
+  description = "ssh-keygen -t ed25519"
+}
